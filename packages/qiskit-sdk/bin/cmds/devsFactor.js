3d59897875a3ea6be6efdbde278ae4e6fe6077bd
Jesús Pérez <jesusprubio@gmail.com>
2018-08-14 08:41:15 +0200

    New command into the SDK: devs-factor

---
+++ packages/qiskit-sdk/bin/cmds/devsFactor.js
@@ -0,0 +1,41 @@
+/**
+ * @license
+ *
+ * Copyright (c) 2017, IBM.
+ *
+ * This source code is licensed under the Apache License, Version 2.0 found in
+ * the LICENSE.txt file in the root directory of this source tree.
+ */
+
+'use strict';
+
+const qiskit = require('../..');
+const logger = require('../lib/logger');
+
+exports.command = 'devs-factor <number>';
+
+exports.aliases = ['df'];
+
+exports.desc = 'Calculate a factor of a number';
+
+exports.builder = {
+  number: {
+    desc: 'Number to factorize',
+    type: 'number',
+  },
+};
+
+exports.handler = argv => {
+  logger.title(qiskit.version);
+
+  qiskit.devs
+    .factor(argv.number)
+    .then(res => {
+      logger.resultHead();
+      logger.json(res);
+    })
+    .catch(err => {
+      logger.error('Making the request', err);
+      process.exit(1);
+    });
+};
