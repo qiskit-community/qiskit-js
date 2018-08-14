c7a2cbbdbb9c6b1ec962dc12e46893d53f8a5210
Jesús Pérez <jesusprubio@gmail.com>
2018-08-14 08:41:01 +0200

    New command into the SDK: cloud-credits

---
+++ packages/qiskit-sdk/bin/cmds/cloudCredits.js
@@ -0,0 +1,36 @@
+/**
+ * @license
+ *
+ * Copyright (c) 2017, IBM.
+ *
+ * This source code is licensed under the Apache License, Version 2.0 found in
+ * the LICENSE.txt file in the root directory of this source tree.
+ */
+
+'use strict';
+
+const qiskit = require('../..');
+const logger = require('../lib/logger');
+
+exports.command = 'cloud-credits';
+
+exports.aliases = ['ccr'];
+
+exports.desc = 'Information about your credits';
+
+exports.builder = {};
+
+exports.handler = () => {
+  logger.title(qiskit.version);
+
+  global.qiskit.cloud
+    .credits()
+    .then(res => {
+      logger.resultHead();
+      logger.json(res);
+    })
+    .catch(err => {
+      logger.error('Making the request', err);
+      process.exit(1);
+    });
+};
